e6b18f10c6dcfb91888b4c6f090ea78f48f81c6a
bhmberkan <berkanburakturgut@gmail.com>
2024-08-26 17:00:07 +0300

    asansoroyunu

---
+++ asansoroyunu/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace asansoroyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            stop();
+            timer4.Start();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            stop();
+            timer3.Start();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            stop();
+            timer2.Start();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            stop();
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Top<238)
+            { pictureBox1.Top += 1;
+            if(pictureBox1.Top>238)
+                { timer1.Stop(); }      
+                        
+            }
+            if (pictureBox1.Top > 238)
+            {
+                pictureBox1.Top -= 1;
+                if (pictureBox1.Top == 238)
+                { timer1.Stop(); }
+
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Top < 192)
+            {
+                pictureBox1.Top += 1;
+                if (pictureBox1.Top > 192)
+                { timer1.Stop(); }
+
+            }
+            if (pictureBox1.Top > 192)
+            {
+                pictureBox1.Top -= 1;
+                if (pictureBox1.Top == 192)
+                { timer1.Stop(); }
+
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Top < 128)
+            {
+                pictureBox1.Top += 1;
+                if (pictureBox1.Top > 128)
+                { timer1.Stop(); }
+
+            }
+            if (pictureBox1.Top > 128)
+            {
+                pictureBox1.Top -= 1;
+                if (pictureBox1.Top == 128)
+                { timer1.Stop(); }
+
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Top < 78)
+            {
+                pictureBox1.Top += 1;
+                if (pictureBox1.Top == 78)
+                { timer1.Stop(); }
+
+            }
+            if (pictureBox1.Top > 78)
+            {
+                pictureBox1.Top -= 1;
+                if (pictureBox1.Top == 78)
+                { timer1.Stop(); }
+
+            }
+       }
+        private void stop()
+        {   timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+            timer4.Stop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stop();
+            timer4.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            stop();
+            timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 30;
+            timer2.Interval = 30;
+            timer3.Interval = 30;
+            timer4.Interval = 30;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            stop();
+        }
+    }
+}
---
+++ asansoroyunu/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("asansoroyunu")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("asansoroyunu")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("65105798-2994-4c43-be85-eaea199b2916")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
